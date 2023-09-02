fe055e1265c05962600ce36ddbf6dca06a0b3ba4
su2minig <141402694+su2minig@users.noreply.github.com>
2023-09-02 17:50:27 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+
+const mainContainer = document.getElementById("menu-list");
+BASE_URL ="http://localhost:3000/"
+
+function createmenu(imgurl, foodtitle, foodname){
+const $product = document.createElement("a")
+const img = document.createElement("img");
+const name = document.createElement("div");
+$product.setAttribute('href' ,'https://su2minig.github.io/lets-cooking/'+foodtitle+'.html')
+$product.setAttribute('target', '_blank')
+console.log(foodname);
+name.innerText = foodname;
+img.src =imgurl;
+$product.append(img, name);
+
+return $product
+}
+
+
+
+
+const Allmenu = document.querySelector(".type-all");
+Allmenu.addEventListener('click',allmenu);
+
+
+
+function allmenu(){
+    const foodContainer = document.createElement("div");
+    foodContainer.id = "food"
+    mainContainer.appendChild(foodContainer);
+    fetch(BASE_URL+"alldata").then((res)=>{
+        return res.json();
+    }).then((json)=>{
+        json.forEach(alldata =>{
+            const foodId = alldata.id;
+            const imgurl = alldata.img;
+            const foodtitle = alldata.title;
+            const foodname = alldata.name;
+            const $product = createmenu(imgurl,foodtitle,foodname);
+            foodContainer.appendChild($product);
+            console.log(foodContainer);
+        }
+        )}
+    )
+}
+
