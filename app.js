d5ee432504c7800617240b30a077f56df9ec9623
su2minig <141402694+su2minig@users.noreply.github.com>
2023-09-02 19:25:43 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const img = document.createElement("img");
 const name = document.createElement("div");
 $product.setAttribute('href' ,'https://su2minig.github.io/lets-cooking/'+foodtitle+'.html')
 $product.setAttribute('target', '_blank')
+$product.setAttribute('class', 'menulist')
 console.log(foodname);
 name.innerText = foodname;
 img.src =imgurl;
@@ -18,10 +19,11 @@ return $product
 
 
 
-
+// 클릭이벤트
 const Allmenu = document.querySelector(".type-all");
 Allmenu.addEventListener('click',allmenu);
 
+// ------------
 
 
 function allmenu(){
@@ -32,7 +34,7 @@ function allmenu(){
         return res.json();
     }).then((json)=>{
         json.forEach(alldata =>{
-            const foodId = alldata.id;
+            // foodContainer.innerHTML = ""
             const imgurl = alldata.img;
             const foodtitle = alldata.title;
             const foodname = alldata.name;
