9f4e9a1e9e27ca92ff31bdf5dc69ab7c0a53bdeb
su2minig <141402694+su2minig@users.noreply.github.com>
2023-10-14 13:05:56 +0900

    세이브페이지 모달 및 css 수정

--- savepage/save.js
+++ savepage/save.js
@@ -12,12 +12,14 @@ const getrecipe = () => {
 
         recipeitem.addEventListener('click', (e) => {
             const $recipe = document.querySelector('#container')
-            document.querySelector('.black-bg').classList.add('show-modal')
+            document.querySelector('.white-bg').classList.add('show-modal')
+            document.querySelector('.modal-bg').classList.add('show-modal')
             $recipe.innerText = value['recipe']
         })
         const btnClose = document.querySelector('.close')
         btnClose.addEventListener('click', () =>{
-            document.querySelector('.black-bg').classList.remove('show-modal')
+            document.querySelector('.white-bg').classList.remove('show-modal')
+            document.querySelector('.modal-bg').classList.remove('show-modal')
         })
     })
 }
